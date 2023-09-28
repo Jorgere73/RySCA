be6ed916c8ee2a40cdfc037f18e987e6be443d26
PatataAcustica <100472783@alumnos.uc3m.es>
2023-09-28 17:15:33 +0200

    Cambiado compile.sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-rawnetcc /tmp/ipv4_client arp.c eth.c ipv4.c ipv4_route_table.c ipv4_config.c ipv4_client.c
+#rawnetcc /tmp/ipv4_client arp.c eth.c ipv4.c ipv4_route_table.c ipv4_config.c ipv4_client.c log.c
 
+
+rawnetcc ipv4_client arp.c eth.c ipv4.c ipv4_route_table.c ipv4_config.c ipv4_client.c log.c
 
