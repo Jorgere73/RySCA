92380ddd905d572db8428e544078afc168af3e9c
PatataAcustica <100472783@alumnos.uc3m.es>
2023-09-30 14:16:41 +0200

    Añadido udp

--- udp.c
+++ udp.c
@@ -10,29 +10,30 @@
 #include <timerms.h>
 #include <arpa/inet.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 //¿DIFRENCIAS ENTRE udp_layer Y udp_frame?
-struct udp_layer {//lo q necesita ip para poder funcionar contiene todas las capas de abajo
+typedef struct udp_layer {//lo q necesita ip para poder funcionar contiene todas las capas de abajo
 
     uint16_t src_port;
     ipv4_layer_t *ipv4_layer;//la capa Ipv4 que esta por debajo
 
-} 
+} udp_layer_t;
 struct udp_frame{//cabeceraUDP(src_port,dst_port,length_udp,checksum) + data
     uint16_t src_port; 
     uint16_t dst_port; 
     uint16_t length_udp;//sizeof(cabecera + data) 
     uint16_t checksum;//lo calculamos en la capa UDP y se verifica en el destino
     unsigned char payload[UDP_MTU];
-} 
+} ;
 //En el open inicializamos el layer que va a contener el puerto y un layer de ipv4 para abrir la capa IP
 udp_layer_t *udp_open(uint16_t puerto, char *ip_config, char *ip_route_config){ 
     udp_layer_t *udp_layer = malloc(sizeof(udp_layer_t));
     udp_layer->ipv4_layer = ipv4_open(ip_config,ip_route_config);
-    udp_layer->source_port = puerto;
+    udp_layer->src_port = puerto;
     return udp_layer;
 } 
-int udp_close(udp_layer_t udp_layer){
+int udp_close(udp_layer_t * udp_layer){
     //Cerramos la capa Ipv4
     ipv4_close(udp_layer->ipv4_layer);
     free(udp_layer);
@@ -40,7 +41,7 @@ int udp_close(udp_layer_t udp_layer){
     
 } 
 
-int udp_send(udp_layer_t udp_layer,ipv4_addr_t ip_dest,uint16_t dst_port,unsigned char * payload, int payload_len){//logica de los puertos?hay q pensar hacia abajo---------------------------------------------------------------------------------
+int udp_send(udp_layer_t *udp_layer,ipv4_addr_t ip_dest,uint16_t dst_port,unsigned char * payload, int payload_len){//logica de los puertos?hay q pensar hacia abajo---------------------------------------------------------------------------------
 
     struct udp_frame pkt_udp_send;
     memset(&pkt_udp_send, 0, sizeof(struct udp_frame)); //Limpiamos la estructura para que no haya basura
@@ -50,7 +51,7 @@ int udp_send(udp_layer_t udp_layer,ipv4_addr_t ip_dest,uint16_t dst_port,unsigne
     pkt_udp_send.dst_port= htons(dst_port);
     pkt_udp_send.length_udp=htons((payload_len+UDP_HEADER));
     pkt_udp_send.checksum =ipv4_checksum((unsigned char*) &pkt_udp_send,UDP_HEADER); //PQ SOLO HACEMOS EL CHECKSUM DE LA CABECERA?--------------------------------------------------------------------------------------------------------------------
-    memcpy(udp_frame.data, payload, payload_len);
+    memcpy(pkt_udp_send.payload, payload, payload_len);
 
 //Lanzar ip_send
     int bytes_udp = ipv4_send(udp_layer->ipv4_layer,ip_dest,TYPE_UDP,(unsigned char*)&pkt_udp_send, (payload_len+UDP_HEADER));
@@ -71,7 +72,7 @@ return 0;
 } 
 
 
-int udp_recv(udp_layer_t udp_layer,unsigned char buffer []/*uint16_t src_port*/, ipv4_addr_t sender, int buffer_len, long int timeout){//logica de los puertos?hay q pensar hacia arriba--------------------------------------------------------------------
+int udp_recv(udp_layer_t* udp_layer,unsigned char buffer []/*uint16_t src_port*/, ipv4_addr_t sender, int buffer_len, long int timeout){//logica de los puertos?
 
 
 struct udp_frame pkt_udp_recv;
@@ -89,9 +90,9 @@ struct udp_frame pkt_udp_recv;
     }
     pkt_udp_recv = (struct udp_frame)buffer;//Ver si esto tiene sentido------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- 
 
-  }while(!(pkt_udp_recv.dst_port== udp_layer.src_port));
+  }while(!(pkt_udp_recv.dst_port== udp_layer->src_port));
 
   //Y ahora????
-
+return 0;
 
 }
