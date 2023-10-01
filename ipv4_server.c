32fd7f22912811e796332ece275ec14b353dac87
PatataAcustica <100472783@alumnos.uc3m.es>
2023-10-01 14:51:31 +0200

    cambiado layer

--- ipv4_server.c
+++ ipv4_server.c
@@ -8,7 +8,7 @@ int main(int argc, char* argv[])
 {
     ipv4_addr_t ipdst;
     ipv4_str_addr("192.168.1.127", ipdst);
-    ipv4_layer_t layer;
+    ipv4_layer_t* layer;
     layer = ipv4_open("ipv4_config_server.txt","ipv4_route_table_server.txt");
     ipv4_send(layer, ipdst,6, 0, 0);
 }
